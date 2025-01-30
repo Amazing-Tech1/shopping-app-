17d818f87e86d1279c7844644fd04c7c0e41cf4e
Amazing-Tech1 <kelvinmakinde2@gmail.com>
2025-01-30 18:25:08 +0100

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './ShopContext.jsx'
 import AuthProvider from './AuthContext.jsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
